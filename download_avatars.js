e88b1559cee1a54f4e03ae964c0a40cbbffc378c
eddycheong <cheong.e49@gmail.com>
2017-10-09 22:30:45 +0000

    generate requestURL for getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,12 @@
 var request = require("request");
 
+var GITHUB_USER = "eddycheong";
+var GITHUB_TOKEN = "c869d3e73d0befab413cdf22ff0676d78f9e52ba";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
