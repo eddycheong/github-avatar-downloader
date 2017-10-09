c82bc366f247d0ab19290dad4d35de139731e75c
eddycheong <cheong.e49@gmail.com>
2017-10-09 23:44:37 +0000

    Implement downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var fs = require("fs");
 
 var GITHUB_USER = "eddycheong";
 var GITHUB_TOKEN = "c869d3e73d0befab413cdf22ff0676d78f9e52ba";
@@ -26,3 +27,14 @@ getRepoContributors("jquery", "jquery", function(err, contributors) {
     console.log("Result:", contributor['avatar_url']);
   });
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      console.log(err)
+    })
+    .pipe(fs.createWriteStream(filePath)); // Assumes directory path exist
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466",
+  "avatars/kvirani.jpg");
